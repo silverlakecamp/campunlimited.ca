1262cdb521dca9f3bcdba90994ed504f8dfd5200
David Mosher <davidmosher@gmail.com>
2021-05-20 10:14:42 -0400

    basic coming soon splash page

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;800&display=swap" rel="stylesheet">
+</svelte:head>
+
+<script>
+  import "./../styles/reset.css";
+  import "./../styles/globals.css";
+</script>
+
+<div class="flex-layout">
+  <slot></slot>
+</div>
+
+<style>
+  .flex-layout {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>Camp Unlimited™ by SLWC.ca</title>
+</svelte:head>
+
+<main>
+  <div class="header container">
+    <h1 class="md bold">
+      Coming Soon
+    </h1>
+  </div>
+  <div class="container">
+    <div class="text">
+      <span class="bold uppercase green xs">Silver</span><span class="light green uppercase xs">Lake</span>
+      <div class="bold uppercase lg tight">camp</div>
+      <div class="uppercase sm green tight squish">unlimited.ca</div>
+      <img src="src/img/slwc_alt_icon_color.png" height="100px" alt="slwc logo">
+    </div>
+  </div>
+</main>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px 20px;
+    text-align: center;
+  }
+
+  img {
+    margin-top: 20px;
+  }
+
+  .container > * {
+    width: 100%;
+    max-width: 900px;
+  }
+
+  .header {
+    min-height: 10vw;
+    padding-top: 4rem;
+    justify-content: flex-end;
+    text-transform: uppercase;
+    text-align: center;
+  }
+</style>
